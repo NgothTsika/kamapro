d35a825ada46ebb09aa4b7700d5cb61816c56847
ngothtsika <ngothtsikavivien@gmail.com>
2026-05-01 17:46:53 +0000

    update and add new path

--- clients/kamaweb/next.config.ts
+++ clients/kamaweb/next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/uc?export=view&id=**",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
     ],
   },
 };
